0de648ee6b521bca545cd6bdc00e49a08c29ba2c
XenoLoaf <XenoLoaf@gmail.com>
2022-04-22 14:18:29 -0500

    Cam Movement

---
+++ Assets/CamScript.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamScript : MonoBehaviour
+{
+  public Transform player;
+  public GameObject control;
+  public GameObject cameraMain;
+  public Vector3 offset;
+  
+  void Update () 
+  {
+        control.transform.position = new Vector3 (player.position.x, player.position.y, offset.z);
+        cameraMain.transform.position = new Vector3 (player.position.x, player.position.y, offset.z);
+  }
+}
