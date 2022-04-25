9b8a8a5029d7bfbd8daada542bcbf2d91f305a24
Lifesteal <elioescoubeyrou@gmail.com>
2022-04-25 03:32:34 +0200

    Fixed version WARNING CHECKPOINTS DELETED!
    Everything work correctly but I deleted the checkpoints, sorry ._.

--- Assets/Material/Scripts/PlayerScripts/PlayerScript.cs
+++ Assets/Material/Scripts/PlayerScripts/PlayerScript.cs
@@ -56,6 +56,7 @@ public class PlayerScript : MonoBehaviour
         timeBeforeSkill = skillCD;
 
         transform.position = new Vector3(PlayerPrefs.GetFloat("position.x"), PlayerPrefs.GetFloat("position.y"), PlayerPrefs.GetFloat("position.z"));
+        transform.position = new Vector3(0.42f, -0.98f, 1.13f);
     }
 
     void LateUpdate()
